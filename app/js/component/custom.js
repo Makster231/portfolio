04afd9658a62d256db201843e9f28ce95c5eeb1c
38071 <makster559553@gmail.com>
2020-05-03 16:18:30 +0300

    added start files

---
+++ app/js/component/custom.js
@@ -0,0 +1,6 @@
+(function ($, window, document) {
+  $(() => {
+    //show content after loaded page
+    $("body").css("opacity", "1");
+  });
+})(window.jQuery, window, document);
---
+++ app/js/scripts.js
@@ -0,0 +1,9 @@
+"use strict";
+
+(function ($, window, document) {
+  $(function () {
+    //show content after loaded page
+    $("body").css("opacity", "1");
+  });
+})(window.jQuery, window, document);
+//# sourceMappingURL=scripts.js.map
