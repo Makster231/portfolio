5b45cf3d4dcafb589d6eabc70ca86840428a7514
38071 <makster559553@gmail.com>
2020-05-04 20:30:27 +0300

    added animation on sections "about" and "works"

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -116,10 +116,16 @@ function scrollToSection(count) {
 
       if ($this.is(nav.about)) {
         scrollToSection(0);
+        pages.about.addClass("js_animation-about");
+      } else {
+        pages.about.removeClass("js_animation-about");
       }
 
       if ($this.is(nav.works)) {
         scrollToSection(1);
+        pages.works.addClass("js_animation-works");
+      } else {
+        pages.works.removeClass("js_animation-works");
       }
 
       if ($this.is(nav.costs)) {
