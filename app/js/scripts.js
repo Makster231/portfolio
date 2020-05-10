40291c8efda39ee7f01b48b9a74403214c92c96f
38071 <makster559553@gmail.com>
2020-05-10 20:34:25 +0300

    styled site on mobiles

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -132,92 +132,95 @@ function scrollToSection(count) {
   }
 
   $(function () {});
-})(window.jQuery, window, document);
+})(window.jQuery, window, document); // (function ($, window, document) {
+//   var tsX;
+//   var tsY;
+//   $(document).bind("touchstart", function (e) {
+//     console.log(e);
+//     tsX = e.originalEvent.touches[0].clientX;
+//     tsY = e.originalEvent.touches[0].clientY;
+//   });
+//   $(document).bind("touchend", function (e) {
+//     var teX = e.originalEvent.changedTouches[0].clientX;
+//     var teY = e.originalEvent.changedTouches[0].clientY;
+//     // tsX = Math.floor(tsX);
+//     // tsY = Math.floor(tsY);
+//     // teX = Math.floor(teX);
+//     // teY = Math.floor(teY);
+//     pages.about.removeClass("js_animation-about");
+//     pages.works.removeClass("js_animation-works");
+//     pages.costs.removeClass("js_animation-costs");
+//     pages.contacts.removeClass("js_animation-contacts");
+//     console.log(e);
+//     console.log("tsX: "+tsX);
+//     console.log("tsY: "+tsY);
+//     console.log("teX: "+teX);
+//     console.log("teY: "+teY);
+//     if (0) {
+//       return;
+//     } else if (tsX < teX) {
+//       if (nav.about.hasClass("js_active")) {
+//         scrollToSection(3);
+//         nav.about.removeClass("js_active");
+//         nav.contacts.addClass("js_active");
+//         pages.contacts.addClass("js_animation-contacts");
+//         return;
+//       }
+//       if (nav.works.hasClass("js_active")) {
+//         scrollToSection(0);
+//         nav.works.removeClass("js_active");
+//         nav.about.addClass("js_active");
+//         pages.about.addClass("js_animation-about");
+//         return;
+//       }
+//       if (nav.costs.hasClass("js_active")) {
+//         scrollToSection(1);
+//         nav.costs.removeClass("js_active");
+//         nav.works.addClass("js_active");
+//         pages.works.addClass("js_animation-works");
+//         return;
+//       }
+//       if (nav.contacts.hasClass("js_active")) {
+//         scrollToSection(2);
+//         nav.contacts.removeClass("js_active");
+//         nav.costs.addClass("js_active");
+//         pages.costs.addClass("js_animation-costs");
+//         return;
+//       }
+//     } else {
+//       if (nav.about.hasClass("js_active")) {
+//         scrollToSection(1);
+//         nav.about.removeClass("js_active");
+//         nav.works.addClass("js_active");
+//         pages.works.addClass("js_animation-works");
+//         return;
+//       }
+//       if (nav.works.hasClass("js_active")) {
+//         scrollToSection(2);
+//         nav.works.removeClass("js_active");
+//         nav.costs.addClass("js_active");
+//         pages.costs.addClass("js_animation-costs");
+//         return;
+//       }
+//       if (nav.costs.hasClass("js_active")) {
+//         scrollToSection(3);
+//         nav.costs.removeClass("js_active");
+//         nav.contacts.addClass("js_active");
+//         pages.contacts.addClass("js_animation-contacts");
+//         return;
+//       }
+//       if (nav.contacts.hasClass("js_active")) {
+//         scrollToSection(0);
+//         nav.contacts.removeClass("js_active");
+//         nav.about.addClass("js_active");
+//         pages.about.addClass("js_animation-about");
+//         return;
+//       }
+//     }
+//   });
+//   $(() => {});
+// })(window.jQuery, window, document);
 
-(function ($, window, document) {
-  var ts;
-  $(document).bind("touchstart", function (e) {
-    ts = e.originalEvent.touches[0].clientX;
-  });
-  $(document).bind("touchend", function (e) {
-    var te = e.originalEvent.changedTouches[0].clientX;
-    ts = Math.floor(ts);
-    te = Math.floor(te);
-    pages.about.removeClass("js_animation-about");
-    pages.works.removeClass("js_animation-works");
-    pages.costs.removeClass("js_animation-costs");
-    pages.contacts.removeClass("js_animation-contacts");
-
-    if (ts === te) {
-      return;
-    } else if (ts < te) {
-      if (nav.about.hasClass("js_active")) {
-        scrollToSection(3);
-        nav.about.removeClass("js_active");
-        nav.contacts.addClass("js_active");
-        pages.contacts.addClass("js_animation-contacts");
-        return;
-      }
-
-      if (nav.works.hasClass("js_active")) {
-        scrollToSection(0);
-        nav.works.removeClass("js_active");
-        nav.about.addClass("js_active");
-        pages.about.addClass("js_animation-about");
-        return;
-      }
-
-      if (nav.costs.hasClass("js_active")) {
-        scrollToSection(1);
-        nav.costs.removeClass("js_active");
-        nav.works.addClass("js_active");
-        pages.works.addClass("js_animation-works");
-        return;
-      }
-
-      if (nav.contacts.hasClass("js_active")) {
-        scrollToSection(2);
-        nav.contacts.removeClass("js_active");
-        nav.costs.addClass("js_active");
-        pages.costs.addClass("js_animation-costs");
-        return;
-      }
-    } else {
-      if (nav.about.hasClass("js_active")) {
-        scrollToSection(1);
-        nav.about.removeClass("js_active");
-        nav.works.addClass("js_active");
-        pages.works.addClass("js_animation-works");
-        return;
-      }
-
-      if (nav.works.hasClass("js_active")) {
-        scrollToSection(2);
-        nav.works.removeClass("js_active");
-        nav.costs.addClass("js_active");
-        pages.costs.addClass("js_animation-costs");
-        return;
-      }
-
-      if (nav.costs.hasClass("js_active")) {
-        scrollToSection(3);
-        nav.costs.removeClass("js_active");
-        nav.contacts.addClass("js_active");
-        pages.contacts.addClass("js_animation-contacts");
-        return;
-      }
-
-      if (nav.contacts.hasClass("js_active")) {
-        scrollToSection(0);
-        nav.contacts.removeClass("js_active");
-        nav.about.addClass("js_active");
-        pages.about.addClass("js_animation-about");
-        return;
-      }
-    }
-  });
-  $(function () {});
-})(window.jQuery, window, document);
 
 (function ($, window, document) {
   function navPages() {
